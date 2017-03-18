4daf9bad5570ddfe0dca2ff13fd5b6416e8584af
aekkang <akang@caltech.edu>
2017-03-18 03:18:59 -0700

    debugigng

--- dstar.py
+++ dstar.py
@@ -27,7 +27,7 @@ class D_Star:
     
     # Init function with robot world's decomposition
     # Start (x,y) and Goal (x,y) 
-    def __init__(self, world, world_size, start, goal):
+    def __init__(self, world_size, start, goal):
         World_X = world_size[0]
         World_Y = world_size[1]
         
@@ -45,20 +45,26 @@ class D_Star:
                     for i2 in range(World_X):
                         self.c[j1][i1][j2][i2] = math.hypot(i2 - i1, j2 - j1)
 
-    def cost(self, n1, n2):
-        return c[n2[1]][n2[0]][n1[1]][n1[0]]
+    def cost(self, curr1, curr2):
+        x1, y1 = curr1.loc
+        x2, y2 = curr2.loc
+        return self.c[y2][x2][y1][x1]
 
     def get(self, n):
         return self.world[n[1]][n[0]]
 
     def put(self, n, cell):
         self.world[n[1]][n[0]] = cell
-        self.Pqueue.put( (cell.h, cell) )
 
     def open_get(self):
-        curr = self.Pqueue.get()
+        item = self.Pqueue.get()
+        curr = self.get(item[1])
 
-        return (curr[0], self.get(curr[1]))
+        return (item[0], curr)
+
+    def open_put(self, cell):
+        cell.t = 'o'
+        self.Pqueue.put( (cell.h, cell.loc) )
 
     def get_kmin(self):
         try:
@@ -71,87 +77,125 @@ class D_Star:
         path = [curr]
 
         while True:
-            curr = self.get(curr).b
-            path.append(curr)
-
-            if curr == self.goal:
-                return path
-            elif curr == None:
+            cell = self.get(curr).b
+            
+            if cell == None:
                 return None
+            else:
+                curr = cell.loc
+                path.append(curr)
+
+                if curr == self.goal:
+                    import numpy as np
+                    from copy import deepcopy
+                    print
+                    print "World:"
+                    print "=================================================="
+                    lol = deepcopy(self.world)
+                    for i in range(len(lol)):
+                        for j in range(len(lol[0])):
+                            n = lol[i][j].loc
+                            if n in path:
+                                lol[i][j] = 1
+                            else:
+                                lol[i][j] = 0
+                    print np.array(lol)
+                    print
+
+                    return path
 
     def get_neighbors(self, curr):
         neighbors = []
-        y = curr[0]
-        x = curr[1]
+        loc = curr.loc
+        x = loc[0]
+        y = loc[1]
 
         min_y = max(0, y - 1)
         max_y = min(self.size[1] - 1, y + 1)
         min_x = max(0, x - 1)
         max_x = min(self.size[0] - 1, x + 1)                
         
-        for j in xrange(min_y, max_y):
-            for i in xrange(min_x, max_x):
-                neighbors.append(self.get(i, j))
+        for j in xrange(min_y, max_y + 1):
+            for i in xrange(min_x, max_x + 1):
+                neighbors.append(self.get((i, j)))
 
         return neighbors
 
     ##################################################
 
     def init_path(self):
-        self.put(self.goal, Cell(0, 0, 'g', 'o', self.goal))
+        goal = Cell(0, 0, 'g', 'o', self.goal)
+        self.put(self.goal, goal)
+        self.open_put(goal)
 
         while True:
+            # import numpy as np
+            # from copy import deepcopy
+            # print
+            # print "World:"
+            # print "=================================================="
+            # lol = deepcopy(self.world)
+            # for i in range(len(lol)):
+            #     for j in range(len(lol[0])):
+            #         tag = lol[i][j].t
+            #         if tag == 'c':
+            #             lol[i][j] = 0
+            #         elif tag == 'n':
+            #             lol[i][j] = -1
+            #         else:
+            #             lol[i][j] = 0
+            # print np.array(lol)
+            # print
+
             k_min = self.process_state()
 
-            if k_min == None:
-                return None
-            elif self.get(self.start) == 'c':
+            if self.get(self.start).t == 'c':
                 return self.get_path(self.start)
+            elif k_min == None:
+                return None
 
     def change_map(self, actual_map, curr):
-        x = curr[0]
-        y = curr[1]
-
         for sensed in self.get_neighbors(curr):
-            neighbors = self.get_neighbors(sensed)
-            i, j = neighbor
+            for neighbor in self.get_neighbors(sensed):
+                i, j = neighbor.loc
 
-            if (actual_map[j][i]):
-                for neighbor in neighbors:
+                if (actual_map[j][i]):
                     self.modify_costs(sensed, neighbor, float("inf"))
 
     # Try to navigate through obstacles with actual world map
     def navigate_map(self, curr):
         while True:
             k_min = self.process_state()
-            
+
             if k_min == None:
                 return None
             elif self.get(curr).h <= k_min:
                 return self.get_path(curr)
 
     def modify_costs(self, n1, n2, new_c):
-        c[n2[1]][n2[0]][n1[1]][n1[0]] = new_c
+        x1, y1 = n1.loc
+        x2, y2 = n2.loc
+
+        self.c[y2][x2][y1][x1] = new_c
         
         if n1.t == 'c':
             curr = self.get(n1)
-            self.insert(n1, curr.h)
+            self.insert(curr, curr.h)
 
         return self.get_kmin()
 
-    def insert(n, h_new):
-        if n.t == 'n':
-            n.k = h_new
-        elif n.t == 'o':
-            n.k = min(k, h_new)
-        elif n.t == 'c':
-            n.k = min(n.h, h_new)
+    def insert(self, curr, h_new):
+        if curr.t == 'n':
+            curr.k = h_new
+        elif curr.t == 'o':
+            curr.k = min(curr.k, h_new)
+        elif curr.t == 'c':
+            curr.k = min(curr.h, h_new)
 
-        n.h = h_new
-        n.t = 'o'
-        self.Pqueue.put( (n.h, n.loc) )
+        curr.h = h_new
+        self.open_put(curr)
 
-    def process_state():
+    def process_state(self):
         try:
             k_old, curr = self.open_get()
         except:
@@ -182,6 +226,8 @@ class D_Star:
                 and neighbor.t == 'c' and neighbor.h > k_old:
                     self.insert(neighbor, neighbor.h)
 
+        curr.t = 'c'
+
         return self.get_kmin()
 
     def run(self, actual_map):
@@ -193,13 +239,17 @@ class D_Star:
 
         curr = self.start
         while curr != self.goal:
-            self.change_map(actual_map, curr)
-            path = self.navigate_map(curr)
+            self.change_map(actual_map, self.get(curr))
+            if self.get(curr).t != 'c':
+                path = self.navigate_map(curr)
+            else:
+                path = self.get_path(curr)
 
             if path == None:
                 return None
 
             curr = path[1]
+            print path
             final_path.append(curr)
 
         return final_path
--- main.py
+++ main.py
@@ -5,20 +5,36 @@ Created on Fri Mar 17 2017
 @author: Sith, Andrew
 """
 
+from dstar import D_Star
+
 def main(world):
     actual_map, start, goal = world
-    world_size = (len(world[0]), len(world))
+    world_size = (len(actual_map[0]), len(actual_map))
 
-    d_star = D_star(aorld_size, start, goal)
-    d_star.init_path()
+    d_star = D_Star(world_size, start, goal)
+    path = d_star.run(actual_map)
+    print path
 
 
 environment = [[0 for i in range(21)] for j in range(31)]
 start = (0, 0)
-end = (20, 30)
+goal = (20, 30)
+
+
+
 
 for j in range(31):
-    environment[j][10] = 1
+    if j == 15:
+        continue
+    environment[j][5] = 1
+
+import numpy as np
+print
+print "World:"
+print "=================================================="
+print np.array(environment)
+print
+
 
 world = (environment, start, goal)
 main(world)
