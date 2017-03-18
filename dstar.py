5e65f6be78ccd7b4d54b82bb19270fc0d7826189
aekkang <akang@caltech.edu>
2017-03-17 19:57:11 -0700

    More methods

--- dstar.py
+++ dstar.py
@@ -5,7 +5,6 @@ Created on Thu Mar 16 11:04:31 2017
 @author: Sith, Andrew
 """
 
-import numpy as np
 import math
 import Queue
 
@@ -34,72 +33,100 @@ class D_Star:
         self.start = start
         self.goal = goal
         self.size = world_size
-        self.world = np.array([[Cell(None, None, None, 'n') for i in range(World_X)] for j in range(World_Y)])
-        self.Pqueue = Queue.PriorityQueue()
+        self.world = [[Cell(None, None, None, 'n') for _ in range(World_X)] for _ in range(World_Y)]
+        self.Pqueue = Queue.PriorityQueue() # Open list
+        self.c = [[[[None for _ in range(World_X)] for _ in range(World_Y)]
+                 for _ in range(World_X)] for _ in range(World_Y)] # Edge costs.
+
+    def cost(self, n1, n2):
+        return c[n2[1]][n2[0]][n1[1]][n1[0]]
                        
     def init_path(self):
-        current = self.goal
+        curr = self.goal
         self.world[self.goal[1]][self.goal[0]] = Cell(0, 0, 'g', 'o')
         
-        while (current != self.start):
-            self.init_neighbors(current)
-            current = self.Pqueue.get()
+        while (curr != self.start):
+            try:
+                self.init_neighbors(curr)
+                curr = self.Pqueue.get()
+            except:
+                # No path found
+                return False
 
-        return 0
-        
-    def init_neighbors(self, current):
-        # Limit to world border
-        min_y = max(0, current[1] - 1)
-        max_y = min(self.size[1] - 1, current[1] + 1)
+        return True
 
-        min_x = max(0, current[0] - 1)
-        max_x = min(self.size[0] - 1, current[0] + 1)
+    def get_neighbors(self, curr):
+        neighbors = []
+
+        y = curr[0]
+        x = curr[1]
+
+        min_y = max(0, y - 1)
+        max_y = min(self.size[1] - 1, y + 1)
+        min_x = max(0, x - 1)
+        max_x = min(self.size[0] - 1, x + 1)                
         
         for j in xrange(min_y, max_y):
             for i in xrange(min_x, max_x):
-                if (i,j) == current:
-                    pass
-                else:
-                    k = self.world[current[1]][current[0]][1]
-                    distance = math.hypot(i - current[0], j - current[1]) + k
-                    if (world[j][i].k == None or world[j][i].k > distance): 
-                        self.world[j][i] = Cell(distance, distance, current, 'o')
-                        self.Pqueue.put( (distance, (i,j)) )
+                neighbors.append((i, j))
 
-        self.world[current[1]][current[0]].t = 'c'
+        return neighbors
+
+    def init_neighbors(self, curr):
+        # Limit to world border
+        neighbors = self.get_neighbors(curr)
+
+        for (i, j) in neighbors:
+            if (i,j) == curr:
+                pass
+            else:
+                k = self.world[curr[1]][curr[0]][1]
+                dist = math.hypot(i - curr[0], j - curr[1]) + k
+                if (world[j][i].k == None or world[j][i].k > dist): 
+                    self.world[j][i] = Cell(dist, dist, curr, 'o')
+                    self.Pqueue.put( (dist, (i,j)) )
+
+        self.world[curr[1]][curr[0]].t = 'c'
 
         return 0
-                    
-    # Try to navigate through obstacles with actual world map
-    def navigate(self, actual_map):
-        path = []
-        path.append(self.start)
-        current = self.start
-        
-        while (current ! = self.goal):
-            x = current[0]
-            y = current[1]
-            
-            if (actual_map[y][x]):
-                # Update cost and push neightbors onto stack
-                self.world[x][y].h = 10000
 
-                min_y = max(0, y - 1)
-                max_y = min(self.size[1] - 1, y + 1)
-                min_x = max(0, x - 1)
-                max_x = min(self.size[0] - 1, x + 1)                
-                
-                for j in xrange(min_y, max_y):
-                    for i in xrange(min_x, max_x):
-                        self.Pqueue.put( (self.world[j][i].k , (i,j)) )
-                        self.world[j][i].t = 'o'
-                
-            current = self.Pqueue.get()
-            if (self.world[j][i].h == self.world[j][i].k):
-                pass
-            elif (self.world[j][i].h > self.world[j][i].k):
-                
-        return path
+    # Try to navigate through obstacles with actual world map
+    def navigate_map(self, actual_map):
+        curr = self.start
         
-    def run():
-        self.init_neighbors()
+        while (curr ! = self.goal):
+            x = curr[0]
+            y = curr[1]
+            
+            sensed = self.get_neighbors(curr)
+
+            for n_sensed in sensed:
+                i, j = n_sensed
+                neighbors = self.get_neighbors((i, j))
+
+                if (actual_map[j][i]):
+                    for n_neighbor in neighbors:
+                        modify_costs(n_sensed, n_neighbor, float("inf"))
+                
+    def change_map(self, curr):
+        curr = self.goal
+        self.world[self.goal[1]][self.goal[0]] = Cell(0, 0, 'g', 'o')
+        
+        while (curr != self.start):
+            try:
+                self.init_neighbors(curr)
+                curr = self.Pqueue.get()
+            except:
+                # No path found
+                return False
+
+        return True
+                
+    def modify_costs(self, n1, n2, new_c):
+        c[n2[1]][n2[0]][n1[1]][n1[0]] = new_c
+        
+        if n1.t == 'c':
+            self.Pqueue.put( (self.world[j][i].k , (i,j)) )
+            self.world[j][i].t = 'o'
+
+        return sorted(self.Pqueue.queue)[0][0]
